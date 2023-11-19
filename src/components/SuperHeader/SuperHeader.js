4ee4b1b0df0f96ad17879337d23e44259c92f318
Stephen Beattie <stephen.beattie@estimateone.com>
2023-11-19 21:47:55 +1100

    Implement super header as per solution

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -26,10 +26,17 @@ const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  display: flex;
+  gap: 24px;
+  height: 40px;
+  align-items: center;
+  padding: 0 32px;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  // This is a greedy value and will take up as much space as it can.
+  margin-right: auto;
 `;
 
 const HelpLink = styled.a`
